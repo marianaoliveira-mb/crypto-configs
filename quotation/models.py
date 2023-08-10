03cf4b68ab2547008593c87b710505e199deaa90
Mariana Oliveira <mariana.oliveira@mb.com.br>
2023-08-10 15:53:30 -0300

    Models e Migrations add

--- quotation/models.py
+++ quotation/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Quotation(models.Model):
+    crypto = models.CharField(max_length=50, unique=True)
+    price = models.FloatField()
