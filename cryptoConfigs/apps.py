ef7c2b81baff41c2f355fb998bc3d54f92c1bbd8
Mariana Oliveira <mariana.oliveira@mb.com.br>
2023-08-11 10:12:53 -0300

    Rm a app quotation e add a cryptoConfigs

--- cryptoConfigs/apps.py
+++ cryptoConfigs/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class QuotationConfig(AppConfig):
+class CryptoconfigsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quotation'
+    name = 'cryptoConfigs'
